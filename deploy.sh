93af5ad871b39c984ad17c70687d7cfccd695b2b
Divya270921 <n.divyashree272000@gmail.com>
2024-10-21 13:10:45 +0530

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pull the latest Docker image
+docker pull divyashree27/devops-build:latest
+
+# Stop and remove the old container
+docker stop devops-build || true
+docker rm devops-build || true
+
+# Run the new container
+docker run -d -p 80:80 --name devops-build divyashree27/devops-build:latest
+
